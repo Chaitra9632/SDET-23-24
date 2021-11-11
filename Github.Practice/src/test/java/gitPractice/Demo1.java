1bece884d3e5a872044775f48bfeaaee8721fdf9
Chaitra M <Chaitra M@TYSS-Chaithra>
2021-11-11 12:14:31 +0530

    User 1 First Commit

---
+++ Github.Practice/src/test/java/gitPractice/Demo1.java
@@ -0,0 +1,13 @@
+package gitPractice;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void demo1()
+	{
+		System.out.println("Hi this is User 1 and its my first Commit");
+	}
+
+}
